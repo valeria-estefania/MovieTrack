f22c1e1b172ee79b079e69b25f4498caab3058ee
EtienneGW <mark.paskor10@gmail.com>
2026-03-02 20:29:07 -0500

    Add files via upload

---
+++ lib/screens/user_login.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class UserLogin extends StatefulWidget {
+  const UserLogin({super.key});
+
+  @override
+  State<UserLogin> createState() => _UserLoginState();
+}
+
+class _UserLoginState extends State<UserLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF210635),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF210635),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_left),
+          style: IconButton.styleFrom(
+            backgroundColor: Colors.white,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+
+            // Título
+            const Text(
+              "INICIO DE SESIÓN",
+              style: TextStyle(
+                color: Color(0xFFF5D5E0),
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 40),
+
+            // EMAIL
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "EMAIL",
+                style: TextStyle(color: Color(0xFF6667AB)),
+              ),
+            ),
+            const SizedBox(height: 5),
+
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              cursorColor: Colors.white,
+              decoration: const InputDecoration(
+                labelText: "INGRESA TU EMAIL",
+                labelStyle: TextStyle(color: Color(0xFFF5D5E0)),
+                filled: true,
+                fillColor: Color(0xFF420D4B),
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // CONTRASEÑA
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "CONTRASEÑA",
+                style: TextStyle(color: Color(0xFF6667AB)),
+              ),
+            ),
+            const SizedBox(height: 5),
+
+            TextField(
+              obscureText: true,
+              style: const TextStyle(color: Colors.white),
+              cursorColor: Colors.white,
+              decoration: const InputDecoration(
+                labelText: "INGRESA TU CONTRASEÑA",
+                labelStyle: TextStyle(color: Color(0xFFF5D5E0)),
+                filled: true,
+                fillColor: Color(0xFF420D4B),
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            // BOTÓN
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF7B337E),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 40,
+                  vertical: 12,
+                ),
+              ),
+              child: const Text(
+                "INICIAR SESIÓN",
+                style: TextStyle(color: Color(0xFF6667AB)),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
